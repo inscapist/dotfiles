02c4db26cbdf20889465c408917ac4c710990ef9
Felix <zen9.felix@gmail.com>
2020-02-09 11:12:59 +0800

    Emacs config

---
+++ .doom.d/config.el
@@ -0,0 +1,62 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+(setq user-full-name "Felix"
+      user-mail-address "zen9.felix@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Victor Mono" :size 16))
+(setq doom-themes-treemacs-enable-variable-pitch nil)
+(setq doom-variable-pitch-font (font-spec :family "Victor Mono" :size 15))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-gruvbox)
+
+;;; :lang org
+(setq org-directory "~/Notes/"
+      org-archive-location (concat org-directory "archive/%s::")
+      org-ellipsis " ▼ "
+      org-bullets-bullet-list '("☰" "☱" "☲" "☳" "☴" "☵" "☶" "☷" "☷" "☷" "☷"))
+; (after! org
+;   (add-to-list 'org-modules 'org-habit t))
+
+;; `nil' to disable it:
+;; t, relative, nil
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+(load! "doom-persist-frame")
+
+
+(map! :n "S" #'evil-write
+      :n "v" nil
+      :n "v v" #'evil-window-vsplit
+      :n "q" nil
+      :n "qq" #'evil-quit)
