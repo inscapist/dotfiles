320df80303fecc6757d8e7b8b6d0694f3aa21300
Felix <zen9.felix@gmail.com>
2020-02-23 22:03:49 +0800

    Resume last framesize

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -16,3 +16,22 @@
 (map! :n "S" #'save-buffer
       :n "q" nil
       :n "qq" #'evil-quit)
+
+(when-let (dims (doom-cache-get 'last-frame-size))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+
+(defun save-frame-dimensions ()
+  (doom-cache-set 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)
