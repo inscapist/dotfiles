b6dd8d225bc6101e935cbf74bab76c9bd624c69e
Felix <zen9.felix@gmail.com>
2020-05-25 13:23:02 +0800

    More intuitive workspace keybindings

--- .doom.d/+bindings.el
+++ .doom.d/+bindings.el
@@ -1,6 +1,7 @@
 (map! :n "q" nil
       :n "s" nil)
 
+;; additive sugar
 (map! :n "S" #'save-buffer
       :n "vv" #'evil-window-vsplit
       :n "ss" #'evil-window-split
@@ -14,17 +15,19 @@
  :ne "C-l"   #'evil-window-right
  :ne "C-j"   #'evil-window-down
  :ne "C-k"   #'evil-window-up
- :ne "` h"   #'+workspace/switch-left
- :ne "` l"   #'+workspace/switch-right
- :ne "` j"   #'+workspace/new
- :ne "` k"   #'+workspace/delete
+ :ne "s-h"   #'+workspace/switch-left
+ :ne "s-l"   #'+workspace/switch-right
+ :ne "s-n"   #'+workspace/new
+ :ne "s-w"   #'+workspace/close-window-or-workspace
+ :ne "s-r"   #'+workspace/rename
  (:map evil-treemacs-state-map
   "C-h" #'evil-window-left
   "C-l" #'evil-window-right
-  "` h"   #'+workspace/switch-left
-  "` l"   #'+workspace/switch-right
-  "` j"   #'+workspace/new
-  "` k"   #'+workspace/delete))
+  "s-h"   #'+workspace/switch-left
+  "s-l"   #'+workspace/switch-right
+  "s-n"   #'+workspace/new
+  "s-w"   #'+workspace/close-window-or-workspace
+  "s-r"   #'+workspace/rename))
 
 ;; open terminal in vertical window
 (map! :leader
