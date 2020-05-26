a62141a7f4ef17b29904c1cb3c2b46a2932d6a7a
Felix <zen9.felix@gmail.com>
2020-05-26 23:59:22 +0800

    Improved mapping standards

--- .doom.d/+bindings.el
+++ .doom.d/+bindings.el
@@ -8,41 +8,42 @@
       :n "K" #'evil-scroll-up)
 
  ;; Easier navigation
-(map!
- :ne "C-h"   #'evil-window-left
- :ne "C-l"   #'evil-window-right
- :ne "C-j"   #'evil-window-down
- :ne "C-k"   #'evil-window-up
- :ne "s-h"   #'+workspace/switch-left
- :ne "s-l"   #'+workspace/switch-right
- :ne "s-n"   #'+workspace/new
- :ne "s-w"   #'+workspace/close-window-or-workspace
- :ne "s-r"   #'+workspace/rename)
+(map! :ne "C-h"   #'evil-window-left
+      :ne "C-l"   #'evil-window-right
+      :ne "C-j"   #'evil-window-down
+      :ne "C-k"   #'evil-window-up
+      :ne "s-h"   #'+workspace/switch-left
+      :ne "s-l"   #'+workspace/switch-right
+      :ne "s-n"   #'+workspace/new
+      :ne "s-w"   #'+workspace/close-window-or-workspace
+      :ne "s-r"   #'+workspace/rename)
 
 ;; Add/override treemacs keybindings
-(map!
- (:map treemacs-mode-map
-  "p"     #'treemacs-peek
-  "x"     #'treemacs-collapse-parent-node
-  "X"     #'treemacs-collapse-all-projects
-  "C-h"   #'evil-window-left
-  "C-l"   #'evil-window-right
-  "s-h"   #'+workspace/switch-left
-  "s-l"   #'+workspace/switch-right
-  "s-n"   #'+workspace/new
-  "s-w"   #'+workspace/close-window-or-workspace
-  "s-r"   #'+workspace/rename))
+(map! :map treemacs-mode-map
+      "p"     #'treemacs-peek
+      "x"     #'treemacs-collapse-parent-node
+      "X"     #'treemacs-collapse-all-projects
+      "C-h"   #'evil-window-left
+      "C-l"   #'evil-window-right
+      "s-h"   #'+workspace/switch-left
+      "s-l"   #'+workspace/switch-right
+      "s-n"   #'+workspace/new
+      "s-w"   #'+workspace/close-window-or-workspace
+      "s-r"   #'+workspace/rename)
 
 ;; Add org-agenda keybindings
-(map!
- (:map org-agenda-mode-map
-  "C-h"   #'evil-window-left
-  "C-l"   #'evil-window-right
-  "s-h"   #'+workspace/switch-left
-  "s-l"   #'+workspace/switch-right
-  "s-n"   #'+workspace/new
-  "s-w"   #'+workspace/close-window-or-workspace
-  "s-r"   #'+workspace/rename))
+(map! :after evil-org-agenda
+      (:map evil-org-agenda-mode-map :m "S" nil)
+      (:map org-agenda-mode-map
+        "S"     #'org-save-all-org-buffers
+        "r"     #'org-agenda-filter-remove-all
+        "C-h"   #'evil-window-left
+        "C-l"   #'evil-window-right
+        "s-h"   #'+workspace/switch-left
+        "s-l"   #'+workspace/switch-right
+        "s-n"   #'+workspace/new
+        "s-w"   #'+workspace/close-window-or-workspace
+        "s-r"   #'+workspace/rename))
 
 
 ;; Open terminal in vertical window
