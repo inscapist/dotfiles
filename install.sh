de329f97cd640cbefc47f9ea9cc3c056349cc681
Felix <zen9.felix@gmail.com>
2020-05-29 12:55:21 +0800

    Using yabai for fun and profit

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/dotfiles/.ctags.d ~/.ctags.d
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.skhdrc ~/.skhdrc
+ln -s ~/dotfiles/.yabairc ~/.yabairc
 
 # git
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
