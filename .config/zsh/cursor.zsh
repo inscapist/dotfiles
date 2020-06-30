8dea89ad89628c7bfbf3c316e5a4bea760a8ee39
Felix <zen9.felix@gmail.com>
2020-06-30 14:01:14 +0800

    Some refactoring

--- .config/zsh/cursor.zsh
+++ .config/zsh/cursor.zsh
@@ -1,13 +1,29 @@
 # Change cursor shape for different vi modes.
+BLOCK='\e[1 q'
+BEAM='\e[5 q'
+
+
+### Init
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne $BEAM
+}
+zle -N zle-line-init
+
+echo -ne $BEAM                  # Use beam shape cursor on startup.
+preexec() { echo -ne $BEAM ;}   # Use beam shape cursor for each new prompt.
+
+
+### OnKeyMap
 function zle-keymap-select {
   if [[ ${KEYMAP} == vicmd ]] ||
      [[ $1 = 'block' ]]; then
-    echo -ne '\e[1 q'
+    echo -ne $BLOCK
   elif [[ ${KEYMAP} == main ]] ||
        [[ ${KEYMAP} == viins ]] ||
        [[ ${KEYMAP} = '' ]] ||
        [[ $1 = 'beam' ]]; then
-    echo -ne '\e[5 q'
+    echo -ne $BEAM
   fi
 
   # == starship specific stanza ==
@@ -16,11 +32,3 @@ function zle-keymap-select {
   # == starship specific stanza ==
 }
 zle -N zle-keymap-select
-zle-line-init() {
-    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
-}
-zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
-
