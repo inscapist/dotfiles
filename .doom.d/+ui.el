8014804d23dfc02faf41d5d2dd669cedcedff37b
Felix <zen9.felix@gmail.com>
2020-07-08 10:47:49 +0800

    Scientism

--- .doom.d/+ui.el
+++ .doom.d/+ui.el
@@ -1,8 +1,9 @@
 ;;; $DOOMDIR/+ui.el -*- lexical-binding: t; -*-
 
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-nord)
 (setq doom-themes-enable-bold t)
-(setq doom-font (font-spec :family "Dank Mono" :size 17 :weight 'semi-light))
+(setq doom-themes-enable-italic t)
+(setq doom-font (font-spec :family "Lekton" :size 17 :weight 'semi-light))
 (setq doom-variable-pitch-font (font-spec :family "Abel" :size 16 :weight 'light))
 (setq all-the-icons-scale-factor 0.8)
 (setq org-superstar-headline-bullets-list '("☯" "☰" "☱" "☲" "☳" "☴" "☵" "☶" "☷"))
