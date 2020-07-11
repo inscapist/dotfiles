c09405c35eff1dcc99fc063284548a3082046026
Felix <zen9.felix@gmail.com>
2020-07-11 17:03:17 +0800

    Install plantuml

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -143,7 +143,7 @@
        org               ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
-       ;;plantuml          ; diagrams for confusing people more
+       plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
