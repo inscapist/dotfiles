e07c3efb5b7e2690cb49177d4b8be9574fa1f28b
Felix <zen9.felix@gmail.com>
2020-07-23 21:26:12 +0800

    Pylance is incredibly fast

--- .doom.d/+langs.el
+++ .doom.d/+langs.el
@@ -1,4 +1,18 @@
 ;;; $DOOMDIR/+langs.el -*- lexical-binding: t; -*-
 
-(after! lsp-python-ms
-  (set-lsp-priority! 'mspyls 1))
+;; (after! lsp-python-ms
+;;   (set-lsp-priority! 'mspyls 1))
+
+(after! lsp-mode
+  (setq lsp-pylance-ms-executable "~/dotfiles/bin/pylance")
+
+  (lsp-register-client
+   (make-lsp-client
+    :new-connection (lsp-stdio-connection (lambda () lsp-pylance-ms-executable)
+                                          (lambda () (f-exists? lsp-pylance-ms-executable)))
+    :major-modes '(python-mode)
+    :server-id 'mspylance
+    :priority 3
+    :initialized-fn (lambda (workspace)
+                      (with-lsp-workspace workspace
+                        (lsp--set-configuration (lsp-configuration-section "python")))))))
