c1ca7ef044ca5ebab6ab05c1bc4942cfcafa2f35
Felix <zen9.felix@gmail.com>
2020-07-31 23:12:26 +0800

    Better org-mode faces

--- .doom.d/+ui.el
+++ .doom.d/+ui.el
@@ -9,21 +9,29 @@
 ;; M-x: describe-face
 ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Face-Attributes.html
 (custom-set-faces!
-  '(bold :family "Monoid" :height 0.8)
-  '(link :slant italic :width condensed)
+  '(bold :family "Ubuntu Mono" :weight semi-bold :height 1.0)
+  '(link :family "Ubuntu Mono" :slant italic :weight light)
   '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic)
-  '(org-todo :inherit 'bold)
+  '(org-todo :inherit bold)
   '(org-agenda-structure :slant italic)
-  '(org-document-title :slant italic))
+  '(org-document-title :slant italic)
+  '(outline-1 :family "Ubuntu Mono" :weight semi-bold :height 1.1)
+  '(outline-2 :family "Ubuntu Mono" :weight normal :foreground "#e39b7b")
+  '(outline-3 :family "Ubuntu Mono" :weight normal)
+  '(outline-4 :family "Ubuntu Mono" :weight semi-light)
+  '(outline-5 :family "Ubuntu Mono" :weight semi-light)
+  '(outline-6 :family "Ubuntu Mono" :weight semi-light)
+  '(outline-7 :family "Ubuntu Mono" :weight semi-light)
+  '(outline-8 :family "Ubuntu Mono" :weight semi-light))
 
 ;; Make doom-modeline slightly smaller than editor font
 (after! doom-modeline
   (custom-set-faces
-   '(mode-line ((t (:family "Advent Pro" :height 0.85))))
-   '(mode-line-inactive ((t (:family "Advent Pro" :height 0.85))))))
+   '(mode-line ((t (:family "Advent Pro" :height 0.9))))
+   '(mode-line-inactive ((t (:family "Advent Pro" :height 0.9))))))
 
-(setq doom-font (font-spec :family "Dank Mono" :size 16 :weight 'light))
+(setq doom-font (font-spec :family "Dank Mono" :size 16 :weight 'semi-light))
 (setq doom-variable-pitch-font (font-spec :family "Advent Pro" :size 16 :weight 'light))
 (setq all-the-icons-scale-factor 0.8)
 (setq org-superstar-headline-bullets-list '("#"))
