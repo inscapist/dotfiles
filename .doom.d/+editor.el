2775bbf437ed93c284bd361a19bdf8e02ffbc5e9
Felix <zen9.felix@gmail.com>
2020-08-24 12:52:17 +0800

    Use scratch buffer to eval elisp by default

--- .doom.d/+editor.el
+++ .doom.d/+editor.el
@@ -4,14 +4,11 @@
 (setq evil-split-window-below t
       evil-vsplit-window-right t)
 
-
 ;; Silence all that useless output
 (setq direnv-always-show-summary nil)
 
-
-;; Do not display line number
+;; Display line number
 (setq display-line-numbers-type t)
-
 
 ;; History & backup settings (save nothing, that's what git is for)
 (setq auto-save-default nil
@@ -19,18 +16,17 @@
       history-length 500
       make-backup-files nil)
 
-
 ;; Avy is the best
 (setq avy-all-windows t)
-
 
 ; Lsp optimization
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
 
-
 ; disable symbol highlighting
 (setq lsp-enable-symbol-highlighting nil)
 
+;; start with lisp interaction mode in scratch buffer
+(setq doom-scratch-initial-major-mode 'lisp-interaction-mode)
 
 ;; customize treemacs
 (after! treemacs
@@ -43,8 +39,8 @@
        (cl-search ".pytest_cache" absolute-path)))
     (add-to-list 'treemacs-ignored-file-predicates #'treemacs-ignore-python-cache-dirs)))
 
-
 ;; Make evil usable in terminal
 (when (not (display-graphic-p))
   (add-hook 'evil-insert-state-entry-hook (lambda () (send-string-to-terminal "\033[5 q")))
   (add-hook 'evil-normal-state-entry-hook (lambda () (send-string-to-terminal "\033[0 q"))))
+
