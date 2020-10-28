0a79ad84fc87ce5604dffdcdcc720d7439f5c950
Felix <zen9.felix@gmail.com>
2020-10-28 23:21:38 +0800

    Switch from pylance (dependent on VSCode) to Pyright

--- .doom.d/+langs.el
+++ .doom.d/+langs.el
@@ -1,20 +1,22 @@
 ;;; $DOOMDIR/+langs.el -*- lexical-binding: t; -*-
 
-;; (after! lsp-python-ms
-;;   (set-lsp-priority! 'mspyls 1))
 
-(after! lsp-mode
-  (setq lsp-pylance-ms-executable "~/dotfiles/bin/pylance")
+;; ------------------------
+;; if pylance is preferred
+;; ------------------------
+;;
+;; (after! lsp-mode
+;;   (setq lsp-pylance-ms-executable "~/dotfiles/bin/pylance")
 
-  (lsp-register-client
-   (make-lsp-client
-    :new-connection (lsp-stdio-connection (lambda () lsp-pylance-ms-executable)
-                                          (lambda () (f-exists? lsp-pylance-ms-executable)))
-    :major-modes '(python-mode)
-    :server-id 'mspylance
-    :priority 3
-    :initialized-fn (lambda (workspace)
-                      (with-lsp-workspace workspace
-                        (lsp--set-configuration (lsp-configuration-section "python")))))))
+;;   (lsp-register-client
+;;    (make-lsp-client
+;;     :new-connection (lsp-stdio-connection (lambda () lsp-pylance-ms-executable)
+;;                                           (lambda () (f-exists? lsp-pylance-ms-executable)))
+;;     :major-modes '(python-mode)
+;;     :server-id 'mspylance
+;;     :priority 3
+;;     :initialized-fn (lambda (workspace)
+;;                       (with-lsp-workspace workspace
+;;                         (lsp--set-configuration (lsp-configuration-section "python")))))))
 
 (setq web-mode-code-indent-offset 2)
