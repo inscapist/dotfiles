1bb7c649587987a657aee947d553c3392b088d3f
Felix <felix@Xilefian.local>
2020-12-19 18:21:47 +0800

    Remove unused ctags and ptpython

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ ln -sfn ~/dotfiles/.config/alacritty ~/.config/alacritty
 ln -sfn ~/dotfiles/.config/nixpkgs ~/.config/nixpkgs
 ln -sfn ~/dotfiles/.config/nvim ~/.config/nvim
 ln -sfn ~/dotfiles/.config/zsh ~/.config/zsh
-ln -sfn ~/dotfiles/.ctags.d ~/.ctags.d
 ln -sfn ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sfn ~/dotfiles/.skhdrc ~/.skhdrc
 ln -sfn ~/dotfiles/.yabairc ~/.yabairc
@@ -21,6 +20,3 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # git
 # ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 # ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
-
-# mkdir -p ~/Library/Application\ Support/ptpython
-# ln -s ~/dotfiles/.ptpython/config.py ~/Library/Application\ Support/ptpython/config.py
