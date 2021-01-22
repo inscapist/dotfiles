58cdd4e52a00a82cdbd12c036945e59da3e77748
Felix <zen9.felix@gmail.com>
2021-01-22 23:47:53 +0800

    Use golden ratio for zoom.el

--- .doom.d/+ui.el
+++ .doom.d/+ui.el
@@ -49,7 +49,7 @@
 (use-package zoom
   :hook (doom-first-input . zoom-mode)
   :config
-  (setq zoom-size '(0.7 . 0.7)
+  (setq zoom-size '(0.618 . 0.618)
         zoom-ignored-major-modes '(dired-mode vterm-mode help-mode helpful-mode rxt-help-mode help-mode-menu org-mode)
         zoom-ignored-buffer-names '("*doom:scratch*" "*info*" "*helpful variable: argv*")
         zoom-ignored-buffer-name-regexps '("^\\*calc" "\\*helpful variable: .*\\*")
