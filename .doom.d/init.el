6671ca93109f2be6767d7ac5a822f2bb2943ff79
Felix <zen9.felix@gmail.com>
2021-03-31 09:56:42 +0800

    Enhance dired with ranger nav

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -65,7 +65,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +ranger +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
