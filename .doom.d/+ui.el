06864f32ecd9b337265e99f3e929b7b37dab4b67
Felix <zen9.felix@gmail.com>
2021-04-01 13:09:43 +0800

    Subtle font size

--- .doom.d/+ui.el
+++ .doom.d/+ui.el
@@ -4,15 +4,15 @@
 ;; UI variables
 ;; =======================================================================
 
+;; custom variables
 (setq felix/default-font "Ellograph CF"
-      felix/default-font-size 14
+      felix/default-font-size 13
       felix/default-font-weight 'light ;; refer font-weight-table variable
-      felix/ui-font "Unica One"
-      felix/ui-font-size 11
-      felix/ui-font-weight 'light
+      felix/muted-font-height 0.95
       felix/modeline-font "Ellograph CF"
-      felix/modeline-height 0.94)
+      felix/modeline-height 0.95)
 
+;; font settings
 (setq
  ;; ;; choose your pokemon =>
  ;; doom-theme 'doom-miramare
@@ -21,21 +21,20 @@
  doom-themes-enable-bold t
  doom-themes-enable-italic t
  doom-font (font-spec :family felix/default-font :size felix/default-font-size :weight felix/default-font-weight)
- doom-variable-pitch-font (font-spec :family felix/ui-font :size felix/ui-font-size :weight felix/ui-font-weight)
  all-the-icons-scale-factor 0.7)
+
 
 ;; use kaolin theme
 (use-package kaolin-themes
   :config
   (load-theme 'kaolin-mono-dark t)
-  (setq kaolin-themes-italic-comments t)
-  (kaolin-treemacs-theme))
+  (setq kaolin-themes-italic-comments t))
 
 
 ;; customize faces. list with SPC-h-F
 (custom-set-faces!
-  `(font-lock-comment-face :slant italic :weight thin)
-  `(font-lock-keyword-face :slant italic :weight thin :height 1.0)
+  `(font-lock-comment-face :slant italic :weight thin :height ,felix/muted-font-height)
+  `(font-lock-keyword-face :slant italic :weight thin :height ,felix/muted-font-height)
   `(font-lock-function-name-face :slant normal :weight normal)
   `(mode-line :height ,felix/modeline-height :family ,felix/modeline-font)
   `(mode-line-inactive :height ,felix/modeline-height ,felix/modeline-font)
