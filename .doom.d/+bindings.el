5d7999d1b3e16bd62b3581e6331a72602e46f231
Felix <zen9.felix@gmail.com>
2021-05-10 15:57:26 +0800

    Better integration with cleverparens

--- .doom.d/+bindings.el
+++ .doom.d/+bindings.el
@@ -64,4 +64,10 @@
 
 ;; where smartparens lives, summon some cleverness
 ;; Ma, I can now slurp and barf as i pleased T_T
-(add-hook 'smartparens-mode-hook #'evil-cleverparens-mode)
+(add-hook 'clojure-mode-hook #'evil-cleverparens-mode)
+(add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode)
+
+;; force ALT key to be used as meta
+(cond (IS-MAC
+       (setq mac-option-modifier       'meta
+             mac-right-option-modifier 'meta)))
