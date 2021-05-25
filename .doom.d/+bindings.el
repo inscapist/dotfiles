aa370fb65a31dd262b2af662566100c28f7f017c
Felix <zen9.felix@gmail.com>
2021-05-25 20:10:04 +0800

    Enable evil-cleverparens in python mdoe

--- .doom.d/+bindings.el
+++ .doom.d/+bindings.el
@@ -94,6 +94,7 @@
 ;; where smartparens lives, summon some cleverness
 ;; Ma, I can now slurp and barf as i pleased T_T
 (add-hook 'clojure-mode-hook #'evil-cleverparens-mode)
+(add-hook 'python-mode-hook #'evil-cleverparens-mode)
 (add-hook 'emacs-lisp-mode-hook #'evil-cleverparens-mode)
 
 ;; force ALT key to be used as meta
