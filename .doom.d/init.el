89e08b1d9d1c768e978ece27a71cfb1d1c23b199
Felix <zen9.felix@gmail.com>
2021-05-29 21:16:53 +0800

    Enable precognitive Ivy

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -23,7 +23,7 @@
        company           ; the ultimate code completion backend
        ;;(helm +childframe)              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +prescient)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
